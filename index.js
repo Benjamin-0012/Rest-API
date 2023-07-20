9071f8357bc300ab3dc3953b3b82eefee690b9bf
Wanjala Benjamin Emmanuel <121640490+Benjamin-0012@users.noreply.github.com>
2023-07-20 21:49:57 +0300

    Added index.js

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Members API Routes
 app.use('/api/members', require('./routes/api/members'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT);
